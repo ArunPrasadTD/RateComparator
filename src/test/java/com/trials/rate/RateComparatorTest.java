8704d6bb329fa859a7b8f344669a8f3d0f160696
ArunPrasadTD <tdarunprasad11@gmail.com>
2022-03-29 20:35:48 +0530

    Initial commit

--- src/test/java/com/trials/rate/RateComparatorTest.java
+++ src/test/java/com/trials/rate/RateComparatorTest.java
@@ -2,6 +2,7 @@ package com.trials.rate;
 
 import com.enhanced.*;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -16,6 +17,7 @@ public class RateComparatorTest {
     private static final double DELTA = 1e-15;
 
     @Test
+    @DisplayName("Displays sorted interest rates")
     public void testMultiBankRatesComparison() {
         ICalculationRequest request = RequestCreator.create(5,25000.0, InterestType.Home, false, true);
         List<Banks> banks = Arrays.asList(Banks.values());
